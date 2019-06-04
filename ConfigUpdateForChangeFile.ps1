c3ecf468bbcd7181f8c0f0a535a9d4e87f150b5f
Cowart <brandon_cowart@bridge360.com>
2019-06-04 11:50:19 -0500

    New functions, code cleanup, and ready to add to C# proj

--- ConfigUpdateForChangeFile.ps1
+++ ConfigUpdateForChangeFile.ps1
@@ -1,100 +1,294 @@
-# 	Write-Host $contentPath\ "contentssssss"		#C:\Users\COWBRA\Desktop\EOS\rangerutility15\RangerApp\config\
-# 	Write-Host $content[$i] + "contentttttt"	#HT23.conf
-# 	Write-Host $contentPath\$content[$i] + "Now all together"	#C:\Users\COWBRA\Desktop\EOS\rangerutility15\RangerApp\config\FR106.conf HT23.conf Machine.conf none.conf PA2200.conf PA6.conf TPE.conf TPE300.conf[1] + Now all together
-
-
-
-
-cd ".\EOS\rangerutility15\\"
-
 $configsPath = ".\RangerApp\config"
+$changeFilePath = ".\RangerApp\config\ChangeFiles"
 
-$content = gci $configsPath -Filter *.conf 		#FR106.conf HT23.conf Machine.conf none.conf PA2200.conf PA6.conf TPE.conf TPE300.conf 
+$path = Split-Path $MyInvocation.MyCommand.Path
+$configChangeFileGen = ".\RangerApp\Commands\ConfigChangeFileGen.ps1"
 
-$contentPath = Resolve-Path -Path $configsPath
+$allConfigFiles = gci $configsPath -Filter *.conf
 
-function GetBuildTemp
+$materialConfigFiles = gci $configsPath -Filter *.conf  | Where-Object {$_ -notmatch "Machine.conf"}
+
+$inputRange = 2..150
+$randomRange = $inputRange | Where-Object { $exclude -notcontains $_ }
+$index = Get-Random -InputObject $RandomRange
+
+$warnUser = Write-Warning "Would you like to reset the config file updates in your working tree? Press H to keep your changes" -WarningAction Inquire
+
+# *** Start Fresh ***
+git checkout RangerApp\config\*
+Remove-Item -Path $changeFilePath -recurse
+# ***
+
+function UpdateValueForSameKey
 {
-    param (
-        [string[]] $fileContent
-    )
-
-	[string[]]$fileContent = Get-Content -Path $contentPath\$f -Encoding UTF8
-    $BuildTemp = [string]($fileContent -match 'build.default.build.temp')
-    $indexOfBuildTemp = [array]::IndexOf($fileContent, $BuildTemp)
-    $BuildValue = ($fileContent[$indexOfBuildTemp] -split "=")[1]
-    return $BuildValue
-}
-
-function UpdateBuildTempValue
-{
-    param (
-        [string] $f
-    )
-
-    Write-Host "Updating build.default.build.temp to 100 in all config files"
-	[string[]]$fileContent = Get-Content -Path $contentPath\$f -Encoding UTF8
-	$BuildTheTemp = GetBuildTemp $fileContent
-	$BuildTemp = [string]($fileContent -match 'build.default.build.temp')
-	# Return -1 if does not exist in this file
-    $indexOfBuildTemp = [array]::IndexOf($fileContent, $BuildTemp)
-    $BuildKey = ($fileContent[$indexOfBuildTemp] -split "=")[0]
-    $BuildValue = ($fileContent[$indexOfBuildTemp] -split "=")[1]
-    # Arbitrary updated value
-    $UpdatedBuildTemp = 100
-
-    # Build temp key does not exist in material file
-    if($indexOfBuildTemp -gt 0)
-	{
-	    $fileContent | ForEach {$_ -replace $BuildValue, $UpdatedBuildTemp} | Set-Content $contentPath\$f
-	    Get-Content $contentPath\$f
-	}
-}
-
-function DeleteRollerSpeedLeft
-{
-    param (
-        [string] $f
-        [string] $FileName
-    )
-    Write-Host "Deleting build.roller.speed.left from all config files"
-	[string[]]$fileContent = Get-Content -Path $contentPath\$f -Encoding UTF8
-
-    [string[]]$NewConfigFile = @()
-    $RollerSpeed = [string]($fileContent -match 'build.roller.speed.left')
-
-    ForEach($f in $fileContent)
-    {
-		$NewConfigFile += $f | where {$_ -ne $RollerSpeed}
-    	#$f | where {$_ -ne $RollerSpeed} | out-file -FilePath RangerApp/config/$PA22000.txt
+    ForEach($f in $allConfigFiles){    
+        [string[]]$fileContent = Get-Content -Path $configsPath\$f -Encoding UTF8
+        $keyBeingUpdated = ($fileContent[$index] -split "=")[0]
+        $fileContent[$index] = "$keyBeingUpdated=$index"
+        $fileContent | Set-Content $configsPath\$f
     }
-    $NewConfigFile | out-file .\\RangerApp\\config\\$FileName
+}
+
+function UpdateValueForSingleMaterialFile
+{
+	$configIndex = Get-Random -Maximum 7
+	$fileBeingUpdated = $materialConfigFiles[$configIndex]
+	[string[]]$fileContent = Get-Content -Path $configsPath\$fileBeingUpdated -Encoding UTF8
+	$keyBeingUpdated = ($fileContent[$index] -split "=")[0]
+	$valueToUpdate = ($fileContent[$index] -split "=")[1]
+	$fileContent[$index] = "$keyBeingUpdated=$index"
+	$fileContent | Set-Content $configsPath\$fileBeingUpdated
+}
+
+function DeleteSameKeyValueAllFiles
+{
+    ForEach($f in $allConfigFiles){    
+        [string[]]$fileContent = Get-Content -Path $configsPath\$f -Encoding UTF8
+        $randomKeyValue = $fileContent[$index]
+
+        $deletingKey = [string]($fileContent -match $randomKeyValue)
+
+        $fileContent | Where-Object {$_ -notmatch $deletingKey} | Set-Content $configsPath\$f
+    }
+}
+
+function DeleteDifferentKeyValuesAllFiles
+{
+    ForEach($f in $allConfigFiles){    
+        [string[]]$fileContent = Get-Content -Path $configsPath\$f -Encoding UTF8
+        $lIndex = Get-Random -Minimum 2 -Maximum 200
+        $randomKeyValue = $fileContent[$lIndex]
+
+        $deletingKey = [string]($fileContent -match $randomKeyValue)
+
+        $fileContent | Where-Object {$_ -notmatch $deletingKey} | Set-Content $configsPath\$f
+    }
 }
 
 function AddNewKeyValue
 {
-    param (
-        [string] $f
-        [string] $FileName
-    )
-	[string[]]$fileContent = Get-Content -Path $contentPath\$f -Encoding UTF8
-	$NewKeyValue = "new.test.key=value"
+    ForEach($f in $allConfigFiles){    
+        [System.Collections.ArrayList]$fileContentList = Get-Content -Path $configsPath\$f -Encoding UTF8
+        $newKeyValue = "new.test.key$index=value"
+        $fileContentList.Insert($index, $newKeyValue)
+        $fileContentList | Set-Content $configsPath\$f
+    }
+}
 
-	$fileContent += $NewKeyValue
-	#Get-Content $contentPath\$f
-	Write-Host $fileContent
+function AddDifferentKeyValueEachFile
+{
+    ForEach($f in $allConfigFiles){    
+        [System.Collections.ArrayList]$fileContentList = Get-Content -Path $configsPath\$f -Encoding UTF8
+        $newKeyValue = "new.test.key$index=value"
+        $fileContentList.Insert(++$index, $newKeyValue)
+        $fileContentList | Set-Content $configsPath\$f
+    }
+}
+
+function RenameSameKeyAllMaterialFiles
+{
+    ForEach($f in $allConfigFiles){
+        [string[]]$fileContent = Get-Content -Path $configsPath\$f -Encoding UTF8
+
+        $randomKeyValue = $fileContent[$index]
+        $updatedKeyName = "build.roller.speed.up$index=$index"
+        $keyBeingRenamed = [string]($fileContent -match $randomKeyValue)
+        $fileContent | ForEach {$_ -replace $keyBeingRenamed, $updatedKeyName} | Set-Content $configsPath\$f
+    }
+}
+
+function AddNewKeyValueSingleMaterialFile
+{
+    $fIndex = Get-Random -Maximum 7  # can get count of avialble material files
+    $newKeyValue = "config.test.key=value"
+    $fileBeingUpdated = $materialConfigFiles[$fIndex]
+    [System.Collections.ArrayList]$fileContentList = Get-Content -Path $configsPath\$fileBeingUpdated -Encoding UTF8
+
+    $fileContentList.Insert(++$index, $newKeyValue)
+    $fileContentList | Set-Content $configsPath\$fileBeingUpdated
+}
+
+function DeleteKeyValueSingleMaterialFile
+{
+    $fileIndex = Get-Random -Maximum 7
+    $keyIndex = Get-Random -Minimum 2 -Maximum 200
+
+    $fileBeingUpdated = $materialConfigFiles[$fileIndex]
+    [string[]]$fileContent = Get-Content -Path $configsPath\$fileBeingUpdated -Encoding UTF8
+    $randomKeyValue = $fileContent[$keyIndex]
+
+ #   $deletingKey = [string]($fileContent -match $randomKeyValue)
+
+    $fileContent | Where-Object {$_ -notmatch $randomKeyValue} | Set-Content $configsPath\$fileBeingUpdated
+}
+
+# Scenarios
+
+function IncrementMajorVersionNumber{
+	$warnUser
+	# Previously this erroneously warned user to add same key/values for all material config files
+    Write-Host -ForegroundColor Yellow "`nTest: Update a value in a single material file and then Delete a single (same) key value in all files (including Machine)"
+    
+	DeleteSameKeyValueAllFiles
+
+    Write-Host -ForegroundColor DarkGray "`nExpected Result:`n"
+    Write-Host -ForegroundColor Green " No errors from the ConfigChangeFileGen tool"
+    Write-Host -ForegroundColor Green " All conf files are listed in the output"
+	Write-Host -ForegroundColor Green " Major Version number (first index) is incremented"
+    Write-Host -ForegroundColor Green " Warning: Please build the solution..."
+    Write-Host -ForegroundColor Green " Total change files count: 8`n"
+    Pause
+}
+
+function IncrementMinorVersionNumber{
+	$warnUser
+	# Add same Key/value pair to each config file to increment Minor number 
+    Write-Host -ForegroundColor Yellow "`nTest: Add same key/value pair in all files (including Machine)`n"
+    
+	AddNewKeyValue
+    
+	Write-Host -ForegroundColor DarkGray "Expected Result:"
+    Write-Host -ForegroundColor Green "`n No errors from the ConfigChangeFileGen tool"
+    Write-Host -ForegroundColor Green "`n The only warning should be to build the solution"
+	Write-Host -ForegroundColor Green "`n Change File Minor number (second index) is incremented by 1 in each file"
+    Write-Host -ForegroundColor Green "`n Total change files count: 8`n"
+
 	Pause
 }
 
-ForEach ($f in $content){
-	$FileName = [System.IO.Path]::GetFileName($f)
-	#Write-Host $fileContent + "All contents of this config file"
 
-	# $UpdateTheBuildTempValue = UpdateBuildTempValue $f
-	# $DeleteTheRollerSpeed = DeleteRollerSpeedLeft $f $FileName
-	$AddNewKeyValue = AddNewKeyValue $f $FileName
+function IncrementPatchNumber{
+	$warnUser
+	# Update a single value for the same key/value pair in all config files
+    Write-Host -ForegroundColor Yellow "`nTest: Update value for same key in all files (including Machine)`n"
+   
+    UpdateValueForSameKey
+    
+	Write-Host -ForegroundColor DarkGray "Expected Result:"
+    Write-Host -ForegroundColor Green "`n No errors from the ConfigChangeFileGen tool"
+    Write-Host -ForegroundColor Green "`n Each file gets a Warning for 'Default' actions. Request to review for 'Forced' actions"
+	Write-Host -ForegroundColor Green "`n Change File Patch number (third index) is incremented by 1 in each file"
+	Write-Host -ForegroundColor Green "`n WARNING to build the solution at the end"
+    Write-Host -ForegroundColor Green "`n Total change files count: 8`n"
+
+	Pause
 }
 
-cd ../..
+function DeleteKeyAllFilesChangeValueSingleFile{
+	$warnUser
 
-Pause
+	# Previously this erroneously warned user to add same key/values for all material config files
+    Write-Host -ForegroundColor Yellow "`nTest: Update a value in a single material file and then Delete a single (same) key value in all files (including Machine)"
+    
+	UpdateValueForSingleMaterialFile
+    DeleteSameKeyValueAllFiles
+
+    Write-Host -ForegroundColor DarkGray "`nExpected Result:`n"
+    Write-Host -ForegroundColor Green " No errors from the ConfigChangeFileGen tool"
+    Write-Host -ForegroundColor Green " All conf files are listed in the output"
+    Write-Host -ForegroundColor Green " Major Version number (first index) is incremented"
+    Write-Host -ForegroundColor Green " Warning: Please build the solution..."
+    Write-Host -ForegroundColor Green " Total change files count: 8`n"
+    Pause
+}
+
+function WarningNewUniqueKeyEachFile{
+	$warnUser
+    Write-Host -ForegroundColor Yellow "`nTest: Add new, unique key to each config file`n"
+
+	AddDifferentKeyValueEachFile
+
+    Write-Host -ForegroundColor Green "Expected Result:"
+    Write-Host -ForegroundColor Green "`n No errors from the ConfigChangeFileGen tool"
+	Write-Host -ForegroundColor Green "`n Change File Minor number (second index) is incremented by 1 in each file"
+    Write-Host -ForegroundColor Green "`n We get WARNING for config files that do not have same 'New' and 'Deleted' actions"
+    Write-Host -ForegroundColor Green "`n Total change files count: 8`n"
+
+	Pause
+}
+
+function WarningDeleteUniqueKeyEachFile
+{
+	$warnUser
+    Write-Host -ForegroundColor Yellow "`nTest: Delete unique key from each config file`n"
+
+	DeleteDifferentKeyValuesAllFiles
+
+    Write-Host -ForegroundColor Green "Expected Result:"
+    Write-Host -ForegroundColor Green "`n No errors from the ConfigChangeFileGen tool"
+	Write-Host -ForegroundColor Green "`n Change File Major number (first index) is incremented by 1 in each file"
+    Write-Host -ForegroundColor Green "`n We get WARNING for config files that do not have same 'New' and 'Deleted' actions"
+    Write-Host -ForegroundColor Green "`n Total change files count: 8`n"
+
+	Pause
+}
+
+function RenameKeyAllFiles{
+
+	$warnUser
+    Write-Host -ForegroundColor Yellow "`nTest: Rename same key from each config file`n"
+
+	RenameSameKeyAllMaterialFiles
+
+    Write-Host -ForegroundColor Green "Expected Result:"
+    Write-Host -ForegroundColor Green "`n No errors from the ConfigChangeFileGen tool"
+	Write-Host -ForegroundColor Green "`n Change File Major number (first index) is incremented by 1 in each file"
+    Write-Host -ForegroundColor Green "`n We get WARNING for config files that do not have same 'New' and 'Deleted' actions. 
+		Review to see if should be 'Renamed'"
+    Write-Host -ForegroundColor Green "`n Total change files count: 8`n"
+
+	Pause
+}
+
+function AddKeySingleMaterialFile{
+
+	$warnUser
+    Write-Host -ForegroundColor Yellow "`nTest: Add a new key/value pair to a single material file`n"
+
+	AddNewKeyValueSingleMaterialFile
+
+
+    Write-Host -ForegroundColor Green "Expected Result:"
+    Write-Host -ForegroundColor Green "`n No errors from the ConfigChangeFileGen tool"
+	Write-Host -ForegroundColor Green "`n Change File Minor number (second index) is incremented by 1"
+    Write-Host -ForegroundColor Green "`n We get WARNING for config files that do not have same 'New' and 'Deleted' actions. 
+		Only 1 out of 7 material config files have the action defined"
+    Write-Host -ForegroundColor Green "`n Total change files count: 1`n"
+
+	Pause
+}
+
+function RunAllActions
+{
+	$warnUser
+    Write-Host -ForegroundColor Yellow "`nTest: Update a value, delete a key/value, rename a key, Add new key to all files`n"
+
+	UpdateValueForSameKey
+	RenameSameKeyAllMaterialFiles
+	DeleteSameKeyValueAllFiles
+	AddNewKeyValue
+
+    Write-Host -ForegroundColor Green "Expected Result:"
+    Write-Host -ForegroundColor Green "`n No errors from the ConfigChangeFileGen tool"
+	Write-Host -ForegroundColor Green "`n Change File Major number (first index) is incremented by 1 in each file"
+    Write-Host -ForegroundColor Green "`n We get WARNING for config files that do not have same 'New' and 'Deleted' actions. 
+		Prompted to check if these should be 'Renamed'"
+	Write-Host -ForegroundColor Green "`n We get WARNING to build the solution." 
+    Write-Host -ForegroundColor Green "`n Total change files count: 8`n"
+
+	Pause
+}
+
+# *** Function Calls ***
+IncrementMajorVersionNumber
+IncrementMinorVersionNumber
+IncrementPatchNumber
+DeleteKeyAllFilesChangeValueSingleFile
+WarningNewUniqueKeyEachFile
+WarningDeleteUniqueKeyEachFile
+RenameKeyAllFiles
+AddKeySingleMaterialFile
+RunAllActions
+
+# *** Run the Config Change tool ***
+.\RangerApp\Commands\ConfigChangeFileGen.ps1
